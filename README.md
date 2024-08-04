36161bb908db26ac6010e24eb36f2718488cd00a
Lucie CORREIA <lucie.correia@telecomnancy.eu>
2024-08-04 18:45:55 +0200

    end of proj with md

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Meteoapp
 
-Ce projet react utilise une API météo pour afficher les données météorologiques.
+Ce projet react utilise une API météo pour afficher les données météorologiques selon la ville la plus proche (géolocalisation avec l'addresse IP).\
+On a la ville, le pays, la température actuelle (en degré Celsius évidemment), la vitesse du vent (en km/h) et le pourcentage d'humidité.
 
+L'API utilisé est celle de "iqair.com".
+
+![Page d'accueil](homepage.png)
--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
 import { useEffect,useState } from "react";
 import loader from "./assets/loader.svg";
+import browser from "./assets/browser.svg";
 import "./App.css";
 const API_KEY = import.meta.env.VITE_API_KEY; // Pour importer une variable d'environnement, ici la clé
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
+  const [errorInfo, setErrorInfo] = useState(null);
   // rappel : useState permet de créer un état local
 
+
   useEffect(() => {
-    fetch(`http://api.airvisual.com/v2/nearest_city?key=${API_KEY}`)
+    fetch(`https://api.airvisual.com/v2/nearest_city?key=${API_KEY}`)
     .then(response => {
+      if (!response.ok) throw new Error(`Error ${response.status}`) // mauvais url etc
+
       return response.json(); // On récupère la réponse en JSON
     })
     .then(responseData =>{
-      // console.log(responseData); //chercher l'imbrication
+      console.log(responseData); //chercher l'imbrication
       setWeatherData({
         city: responseData.data.city,
         country: responseData.data.country,
         temperature: responseData.data.current.weather.tp,
-        icon : responseData.data.current.weather.ic
+        humidity: responseData.data.current.weather.hu,
+        windVelocity : (responseData.data.current.weather.ws*3.6).toFixed(1),
+        iconId : responseData.data.current.weather.ic
       });
     })
-  })
+    .catch(error => {
+      setErrorInfo(error.message)
+    })
+
+  }, [])
  
   return (
 
-      // rajoute classe active qd weatherData est null, donc avt la reception
+      // rajoute classe active qd weatherData et errorInfo sont null, donc avt la reception
       <main>
-        <div className={`loader-container ${!weatherData && "active"}`}> 
+        <div className={`loader-container ${(!weatherData && !errorInfo) && "active"}`}> 
           <img src={loader} alt="loading icon"/>
         </div>
         {weatherData && (
@@ -35,14 +46,24 @@ function App() {
         <p className="city-name">{weatherData.city}</p>
         <p className="country-name">{weatherData.country}</p>
         <p className="temperature">{weatherData.temperature}°</p>
+        <p className="wind">- {weatherData.windVelocity} km/h -</p>
+        <p className="humidity">- {weatherData.humidity} % -</p>
+
         <div className="info-icon-container">
-          <img src={`/icons/${weatherData.icon}.svg`} className="info-icon"alt="weather icon"/>
+          <img src={`/icons/${weatherData.iconId}.svg`} className="info-icon"alt="weather icon"/>
         </div>
         </>
         )}
+
+      {(errorInfo && !weatherData) && (
+          <>
+            <p className="error-information">{errorInfo}</p>
+            <img src={browser} alt="error icon"/>
+          </>
+        )}  
       </main>
   
-  );
+  )
 }
 
 export default App;
